f3423d8b1e5deec4729c6e878c6207180070c11a
karthimech <35652925+karthimech@users.noreply.github.com>
2018-02-06 16:22:48 +0530

    Create natural.C

---
+++ natural.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int p,q,r;
+    printf("enter value");
+    scanf("%d",&p);
+    for(r=0;r<=p;r++)
+    {
+        q+=r;
+    }
+    printf("enter natural number=%d",q);
+}
